467c8d0be02adb276339bcd30010ff6722c9c63c
Jake Thomas <hey.jthomas@gmail.com>
2024-12-12 21:49:35 +0000

    removed base path

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: "/frsh-ui",
-  assetPrefix: "/frsh-ui/",
   output: "export",
-  reactStrictMode: true,
 };
 
 export default nextConfig;
