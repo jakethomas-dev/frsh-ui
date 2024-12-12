87b9f1640b0fbb6c9589ee8f020e719b862b9314
Jake Thomas <hey.jthomas@gmail.com>
2024-12-12 21:08:11 +0000

    gh-pages added

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   basePath: "/frsh-ui",
+  assetPrefix: "/frsh-ui/",
   output: "export",
   reactStrictMode: true,
 };
 
-module.exports = nextConfig;
+export default nextConfig;
